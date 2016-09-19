8a76a2f4299b5f6aa4b6fd3a108d3008e01c86dd
Kenold Beauplan <bkenold@gmail.com>
2016-09-18 22:16:18 -0400

    initial commit

---
+++ views/_slider.php
@@ -0,0 +1,6 @@
+<section class="jumbotron text-xs-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">Discover New Music</h1>
+        <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+    </div>
+</section -->
